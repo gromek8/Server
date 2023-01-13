f24ea135832b714cc416c362bf75e87dcba8d1cd
Mateusz Gromek <8mateusz5@gmail.com>
2023-01-13 09:04:19 +0100

    poprawki 13.01

--- Server.cpp
+++ Server.cpp
@@ -9,8 +9,21 @@
 #define HOST "127.0.0.1"
 #define MAX_CLIENTS 10
 
-void handle_connection(SSL* ssl);
 
+enum METHOD {
+	NONE,
+	READ,
+	SEND
+};
+
+struct Data {
+	FILE* file;
+	char message_buffor[4048];
+	METHOD method;
+	SSL* ssl;
+};
+
+int handle_connection(Data* client_data, WSAPOLLFD* fd);
 
 void ShowCerts(SSL* ssl)
 {
@@ -40,53 +53,70 @@ int main()
 	OpenSSL_add_all_algorithms();
 
 	WSADATA wsa_data;
+	// Kontekst ssl
+	SSL_CTX* ctx;
+	// Socket serwera
+	SOCKET sock;
+	// Struktura z informacjami o adresie serwera
+	struct sockaddr_in server_addr;
+	// Struktura z informacjami o aktywności gniazd
+	WSAPOLLFD fds[MAX_CLIENTS + 1];
+	// Tablica informacji o kliencie
+	Data client_data[MAX_CLIENTS];
+
 	if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
 	{
 		printf("WSAStartup failed.\n");
 		return 1;
 	}
 
-	SSL_CTX* ctx;
-	ctx = SSL_CTX_new(TLS_server_method());
+	ctx = SSL_CTX_new(TLS_server_method()); //stworzenie kontekst ssl
 	if (ctx == NULL)
 	{
 		ERR_print_errors_fp(stderr);
-		abort();
-	}
 
-	if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0)
-	{
-		ERR_print_errors_fp(stderr);
-		abort();
-	}
-
-	if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0)
-	{
-		ERR_print_errors_fp(stderr);
-		abort();
-	}
-
-	if (!SSL_CTX_check_private_key(ctx))
-	{
-		fprintf(stderr, "Private key does not match the public certificate\n");
-		abort();
-	}
-	SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL); //ustawienie weryfikacji certyfikatów
-	SSL_CTX_load_verify_locations(ctx, "rootCA.crt", NULL); //ustawienie bazowego certyfikatu
-
-	// Tworzenie gniazda
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET)
-	{
-		printf("socket failed.\n");
 		WSACleanup();
 		return 1;
 	}
 
-	// Struktura z informacjami o adresie serwera
-	struct sockaddr_in server_addr;
+	if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0)// zaczytywanie certyfikatu
+	{
+		ERR_print_errors_fp(stderr);
+		SSL_CTX_free(ctx);
+		WSACleanup();
+		return 1;
+	}
+
+	if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) // zaczytywanie klucza certyfikatu
+	{
+		ERR_print_errors_fp(stderr);
+		SSL_CTX_free(ctx);
+		WSACleanup();
+		return 1;
+	}
+
+	if (!SSL_CTX_check_private_key(ctx)) // weryfikacja poprawności certyfikatu
+	{
+		fprintf(stderr, "Private key does not match the public certificate\n");
+		SSL_CTX_free(ctx);
+		WSACleanup();
+		return 1;
+	}
+	SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);//ustawienie weryfikacji certyfikatów
+	SSL_CTX_load_verify_locations(ctx, "rootCA.crt", NULL); //ustawienie bazowego certyfikatu
+
+	// Tworzenie gniazda
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET)
+	{
+		printf("socket failed.\n");
+		SSL_CTX_free(ctx);
+		WSACleanup();
+		return 1;
+	}
+
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_addr.sin_addr.s_addr = inet_addr(HOST);
 	server_addr.sin_port = htons(PORT);
 
 	// Przypisywanie gniazda do struktury adresowej
@@ -94,6 +124,7 @@ int main()
 	{
 		printf("bind failed.\n");
 		closesocket(sock);
+		SSL_CTX_free(ctx);
 		WSACleanup();
 		return 1;
 	}
@@ -103,16 +134,17 @@ int main()
 	{
 		printf("listen failed.\n");
 		closesocket(sock);
+		SSL_CTX_free(ctx);
 		WSACleanup();
 		return 1;
 	}
+	//czyszczenie tablicy danych
+	for (int i = 0; i < MAX_CLIENTS; i++) {
+		client_data[i].ssl = NULL;
+		client_data[i].file = NULL;
+	}
 
-	// Tablica gniazd klientów
-	SSL* ssl_clients[MAX_CLIENTS];
-	for (int i = 0; i < MAX_CLIENTS; i++)
-		ssl_clients[i] = NULL;
-	// Struktura z informacjami o aktywności gniazd
-	WSAPOLLFD fds[MAX_CLIENTS + 1];  //pierwszy przypisany do serwera
+
 	while (1)
 	{
 		fds[0].fd = sock;
@@ -122,13 +154,7 @@ int main()
 		// Przepisanie gniazd klientów do struktury
 		for (int i = 0; i < MAX_CLIENTS; i++)
 		{
-			if (ssl_clients[i])  //przypisanie klientów do puli
-			{
-				fds[i + 1].fd = SSL_get_fd(ssl_clients[i]);
-				fds[i + 1].events = POLLRDNORM;
-				fds[i + 1].revents = 0;
-			}
-			else
+			if (!client_data[i].ssl)
 			{
 				fds[i + 1].fd = INVALID_SOCKET;
 				fds[i + 1].events = 0;
@@ -137,7 +163,7 @@ int main()
 		}
 
 		// Oczekiwanie na aktywność gniazd
-		int ret = WSAPoll(fds, MAX_CLIENTS + 1, -1); //nasłuchiwanie 
+		int ret = WSAPoll(fds, MAX_CLIENTS + 1, 5000);//nasłuchiwanie
 		if (ret == SOCKET_ERROR)
 		{
 			printf("WSAPoll failed.\n");
@@ -147,7 +173,7 @@ int main()
 		// Sprawdzenie aktywności gniazda nasłuchującego
 		if (fds[0].revents & POLLRDNORM)
 		{
-			// Struktura z informacjami o adresie klienta, tworzenie
+			// Struktura z informacjami o adresie klienta
 			struct sockaddr_in client_addr;
 			int client_addr_len = sizeof(client_addr);
 
@@ -189,7 +215,7 @@ int main()
 			ShowCerts(ssl_client);
 
 			// Weryfikacja certyfikatu klienta
-			X509* client_cert = SSL_get_peer_certificate(ssl_client); //zaciągniecie certyfikatu
+			X509* client_cert = SSL_get_peer_certificate(ssl_client);//zaciągniecie certyfikatu
 			if (!client_cert)
 			{
 				printf("SSL_get_peer_certificate failed.\n");
@@ -218,7 +244,7 @@ int main()
 			int free_index = -1;
 			for (int i = 0; i < MAX_CLIENTS; i++)
 			{
-				if (!ssl_clients[i])
+				if (!client_data[i].ssl)
 				{
 					free_index = i;
 					break;
@@ -235,133 +261,157 @@ int main()
 				continue;
 			}
 			// Dodanie nowego klienta do tablicy
-			ssl_clients[free_index] = ssl_client;
+			client_data[free_index].ssl = ssl_client;
+			client_data[free_index].method = NONE;
+			fds[free_index + 1].fd = SSL_get_fd(ssl_client);
+			fds[free_index + 1].events = POLLRDNORM;
 		}
 
 		// Obsługa aktywności gniazd klientów
-		for (int i = 0; i < MAX_CLIENTS; i++) 
+		for (int i = 0; i < MAX_CLIENTS; i++)
 		{
 			if (fds[i + 1].revents & POLLRDNORM)
-				handle_connection(ssl_clients[i]);
+				if (handle_connection(&client_data[i], &fds[i + 1]) == -1) { // klient chce coś wysłać
+					printf("Wystąpił błąd");
+				};
+			if (fds[i + 1].revents & POLLWRNORM) {
+				if (handle_connection(&client_data[i], &fds[i + 1]) == -1) { //klient chce coś zaczytać
+					printf("Wystąpił błąd");
+				};
+			}
 		}
 	}
 
 	// Zwalnianie zasobów i zamykanie gniazda
 	for (int i = 0; i < MAX_CLIENTS; i++)
 	{
-		if (ssl_clients[i])
+		if (client_data[i].ssl)
 		{
-			SSL_shutdown(ssl_clients[i]);
-			SSL_free(ssl_clients[i]);
+			SSL_shutdown(client_data[i].ssl);
+			SSL_free(client_data[i].ssl);
+			fclose(client_data[i].file);
 		}
 	}
+
+	SSL_CTX_free(ctx);
 	closesocket(sock);
 	WSACleanup();
 
 	return 0;
 }
 
-void handle_connection(SSL* ssl)
+int handle_connection(Data* client_data, WSAPOLLFD* fd)
 {
-	// Odbieranie certyfikatu klienta
-	char buffer[1024];
-	/*if (SSL_read(ssl, buffer, sizeof(buffer)) <= 0)
-	{
-		printf("SSL_read failed.\n");
-		return;
-	}
+	char buffer[1025];
 
 	// Weryfikacja certyfikatu klienta
-	X509* cert = SSL_get_peer_certificate(ssl);
+	X509* cert = SSL_get_peer_certificate((*client_data).ssl);
 	if (!cert)
 	{
 		printf("Could not get client certificate.\n");
-		return;
+		return -1;
 	}
 
-	long result = SSL_get_verify_result(ssl);
+	long result = SSL_get_verify_result((*client_data).ssl);
 	if (result != X509_V_OK)
 	{
 		printf("Certificate verification failed.\n");
 		X509_free(cert);
-		return;
+		return -1;
 	}
+
+	(*fd).events = POLLRDNORM; //ustawienie flagi na otwarcie wysyłki
 
 	// Zwalnianie certyfikatu
-	X509_free(cert);*/
+	X509_free(cert);
+	if ((*client_data).method == READ) {
+		int n = fread(buffer, 1, 1024, (*client_data).file); //jeżeli przeczyta, wysyła dane do klienta
+		if (n > 0)
+			if (SSL_write((*client_data).ssl, buffer, n) != n) {
+				fclose((*client_data).file);
+				(*client_data).method = NONE;
+				perror("Nie udało się wysłać danych do klienta");
+				return -1;
+			}
+			else {
 
-	// Pętla obsługi komend
-	while (1)
-	{
-		// Odbieranie komendy od klienta
-		if (SSL_read(ssl, buffer, sizeof(buffer)) <= 0)
-		{
-			printf("SSL_read failed.\n");
-			break;
+				(*fd).events = POLLOUT | POLLWRNORM; //ustawienie flagi do zaczytywania
+				return 0;
+			}
+		// Sprawdź, czy nie wystąpił błąd podczas wczytywania danych z pliku
+		if (n < 0) {
+			fclose((*client_data).file);
+			(*client_data).method = NONE;
+			perror("Nie udało się wczytać danych z pliku");
+			return -1;
 		}
+		if (n == 0) {
+			SSL_write((*client_data).ssl, "END_OF_FILE", 11);
 
-		//const char* serverResponse= "Test text dasdasd\n";
-		// 
-		// Obsługa komendy 'send'
-		if (strncmp(buffer, "send", 4) == 0)
-		{
-			// Otwórz plik do zapisu
-			FILE* file = fopen("plik_od_klienta.txt", "wb");
-			if (file == NULL) {
-				perror("Nie udało się otworzyć pliku do zapisu");
-				break;
-			}
-			// Odbieranie danych od klienta
-			int n, htr;
-			while ((n = SSL_read(ssl, buffer, 1024)) > 0) {
-				// Sprawdzanie, czy otrzymano znak konca wysylania danych
-				if (strncmp(buffer, "END_OF_FILE", 11) == 0)
-				{
-					break;
-				}
-				if (fwrite(buffer, 1, n, file) != n) {
-					perror("Nie udało się zapisać danych do pliku");
-					break;
-				}
-			}
-			// Sprawdź, czy nie wystąpił błąd podczas odbierania danych
-			if (n < 0) {
-				printf("SSL_read failed.\n");
-				break;
-			}
-			
-		
+			(*client_data).method = NONE;
+			fclose((*client_data).file);
 
-			// Zamknij plik
-			fclose(file);
-		}
-		// Obsługa komendy 'read'
-		else if (strncmp(buffer, "read", 4) == 0)
-		{
-			// Otwórz plik do odczytu
-			FILE* file = fopen("plik_do_wyslania.txt", "rb");
-			if (file == NULL) {
-				perror("Nie udało się otworzyć pliku do odczytu");
-				break;
-			}
-			// Wczytaj dane z pliku i wyślij je do klienta
-			int n;
-			while ((n = fread(buffer, 1, 1024, file)) > 0) {
-				if (SSL_write(ssl, buffer, n) != n) {
-					perror("Nie udało się wysłać danych do klienta");
-					break;
-				}
-			}
-
-			// Sprawdź, czy nie wystąpił błąd podczas wczytywania danych z pliku
-			if (n < 0) {
-				perror("Nie udało się wczytać danych z pliku");
-				break;
-			}
-			SSL_write(ssl, "END_OF_FILE", 11);
-
-			// Zamknij plik
-			fclose(file);
+			return 0;
 		}
 	}
+	// Odbieranie komendy od klienta
+	int size = SSL_read((*client_data).ssl, buffer, sizeof(buffer) - 1);
+	if (size <= 0)
+	{
+		printf("SSL_read failed.\n");
+		return -1;
+	}
+
+	if (strncmp(buffer, "END_OF_FILE", 11) == 0) //informacja o końcu pliku klienta
+	{
+		fclose((*client_data).file);
+		(*client_data).file = NULL;
+		(*client_data).method = NONE;
+		return 0;
+	}
+
+	buffer[size] = '\0'; //zamkniecie stringa
+
+	// Odbieranie danych od klienta
+	if ((*client_data).method == SEND) { //sprawdza czy klient jest w trakcie wysyłania
+		if (fwrite(buffer, 1, size, (*client_data).file) != size) { //zapis do pliku
+			perror("Nie udało się zapisać danych do pliku");
+			fclose((*client_data).file);
+			(*client_data).file = NULL;
+			(*client_data).method = NONE;
+			return -1;
+		}
+	}
+
+	// Obsługa komendy 'send'
+	else if (strncmp(buffer, "send", 4) == 0)
+	{
+		char file_name[20];
+		sprintf(file_name, "file_%d.txt", (*fd).fd); //generowanie nazwy plików na podtsawie socketa
+		// Otwórz plik do zapisu
+		FILE* file = fopen(file_name, "wb");
+		if (file == NULL) {
+			perror("Nie udało się otworzyć pliku do zapisu");
+			return -1;
+		}
+		(*client_data).method = SEND;
+		(*client_data).file = file;
+	}
+	// Obsługa komendy 'read'
+	else if (strncmp(buffer, "read", 4) == 0)
+	{
+		// Otwórz plik do odczytu
+		FILE* file = fopen("plik_do_wyslania.txt", "rb");
+		if (file == NULL) {
+			perror("Nie udało się otworzyć pliku do odczytu");
+			return -1;
+		}
+		(*client_data).method = READ;
+		(*client_data).file = file;
+		(*fd).events = POLLOUT | POLLWRNORM;
+	}
+	else {
+		printf("Recieved message: %s", buffer);
+	}
+	return 0;
 }
